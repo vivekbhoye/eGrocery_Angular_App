fcee7de026fa0e7158a8a51858dba22c79de8039
va-cell <vaishnavilakshmi29@gmail.com>
2021-08-20 12:43:54 -0700

    by Vaishnavi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+import { ContactusComponent } from './contactus/contactus.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
+import { AddcartComponent } from './addcart/addcart.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    
+    AddcartComponent,
+    LoginComponent,
+    RegistrationComponent,
+    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
